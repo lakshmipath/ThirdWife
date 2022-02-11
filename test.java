9f024432edd09b5387c4b220b8aa7e8178b1a0c7
lakshmipath <97572046+lakshmipath@users.noreply.github.com>
2022-02-10 22:58:59 -0800

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,10 @@ public class test
 {
     public static void main(String args[])
     {
-      System.out.println("welcome");
+      int x=Integer.parseInt(args[0]);
+      int y=Integer.parseInt(args[1]);
+      System.out.println("result:"+(x+y));
+        
     }
 }
   
